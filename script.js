c71b7793031f33077520bdad1d0bb8b138850913
Can Ngo <44318634+nncan99@users.noreply.github.com>
2024-08-01 22:43:38 +0700

    Create boilerplate for project

---
+++ script.js
@@ -0,0 +1,2 @@
+const urlEdu = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json';
+const urlCountry = 'https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json'
